12570cdcc58c7d8e2eecc213a99777d46cf81414
Eoin Pinaqui <45332521+eoinpinaqui@users.noreply.github.com>
2020-08-26 00:18:21 +0100

    Updates

---
+++ precache-manifest.39fe9a33febcb74fb8841f679dc39270.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2265a6fd72252b11011b9ec5565057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91673c42b537abcb885a",
+    "url": "/static/css/main.b08a4515.chunk.css"
+  },
+  {
+    "revision": "069148e066a08264f1a4",
+    "url": "/static/js/2.e6170597.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6170597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91673c42b537abcb885a",
+    "url": "/static/js/main.a725ef38.chunk.js"
+  },
+  {
+    "revision": "42767970418955565418",
+    "url": "/static/js/runtime-main.980bfdaa.js"
+  }
+]);
